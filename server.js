ba9622984e35fba882a49a437b3bea83e772d14e
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-25 14:20:44 -0700

    added paths

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require("express");
 const app = express();
 
 console.log("nodemon is saving lives");
-app.get("/", (req, res) => {
-  res.send("Hello World");
+
+const paths = {
+  saying: "Nobody doesn't like Molten Boron!",
+  popplers: "Pop a Poppler in your mouth when you eat a Fishy Joe's!"
+};
+app.get("/:path", (req, res) => {
+  const path = req.params.path;
+  res.json({
+    statement:
+      paths[path] || "I am Bender, please insert girder (file path not found)"
+  });
 });
+
+// app.get("/saying", (req, res) => {
+//   res.json({ saying: "Nobody doesn't like Molten Boron!" });
+// });
+// app.get("/popplers", (req, res) => {
+//   res.json({
+//     popplers: "Pop a Poppler in your mouth when you eat a Fishy Joe's!"
+//   });
+// });
 const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
