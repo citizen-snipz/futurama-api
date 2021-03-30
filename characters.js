d90c505169d0444a27d64e5e4f029f6ec5cf6e8c
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-29 20:00:12 -0700

    Got server functional, updated characters and css

---
+++ characters.js
@@ -0,0 +1,85 @@
+let characters = [
+  {
+    id: 1,
+    name: "Phillip J. Fry",
+    nickname: "Fry",
+    species: "Human",
+    homePlanet: "Earth",
+    job: "Delivery Boy",
+    quote: "Shut up and take my money!",
+    image: "/img/Philip-J-Fry.png"
+  },
+  {
+    id: 2,
+    name: "Lord Nibbler",
+    nickname: "Nibbler",
+    species: "Planet Eternium",
+    homePlanet: "Niblonia",
+    job: "Undercover agent",
+    quote: "We do live long and are celebrated poopers.",
+    image: "/img/Lord_Nibbler.png"
+  },
+  ,
+  {
+    id: 3,
+    name: "Turanga Leela",
+    nickname: "Leela",
+    species: "Mutant",
+    homePlanet: "Earth",
+    job: "Captain of the Planet Express ship",
+    quote: "Men who call too much are the worst... I bet.",
+    image: "/img/Turanga-Leela.png"
+  },
+  {
+    id: 4,
+    name: "Bender Bending Rodriguez",
+    nickname: "Bender",
+    species: "Robot",
+    homePlanet: "Earth",
+    job: "Planet Express employee and petty criminal",
+    quote: "Bite my shiny metal ass!",
+    image: "/img/Bender_Rodriguez.png"
+  },
+  {
+    id: 5,
+    name: "Professor Hubert J. Farnsworth",
+    nickname: "The Professor",
+    species: "Human",
+    homePlanet: "Earth",
+    job: "CEO of Planet Express",
+    quote: "Good news, everyone!",
+    image: "/img/Professor-Farnsworth.png"
+  },
+  {
+    id: 6,
+    name: "Dr. John A. Zoidberg, MD",
+    nickname: "Zoidberg",
+    species: "Decapodian",
+    homePlanet: "Decapod 10",
+    job: "Doctor",
+    quote: "Why not Zoidberg?",
+    image: "/img/Dr_John_Zoidberg.png"
+  },
+  {
+    id: 7,
+    name: "Hermes Conrad",
+    nickname: "Hermes",
+    species: "Human (suspected outer space potato man)",
+    homePlanet: "Earth",
+    job: "Bureaucrat",
+    quote: "Great something of some place",
+    image: "/img/Hermes.png"
+  },
+  {
+    id: 8,
+    name: "Amy Wong Kroker",
+    nickname: "Amy",
+    species: "Human",
+    homePlanet: "Mars",
+    job: "Graduate student and heiress",
+    quote: "Ew, pukeatronic!",
+    image: "/img/AmyWong.png"
+  }
+];
+
+module.exports = { characters };
--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require("express");
+const { user, mdbKey } = require("./keys.js");
+const { characters } = require("./characters");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect(
+  `mongodb+srv://${user}:${mdbKey}@cluster0.g2mtk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true
+  }
+);
 
-console.log("nodemon is saving lives");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  let { charName } = req.query;
+  charName = charName.toLowerCase();
+  const charResults = characters.filter(
+    (char) =>
+      char.name.toLowerCase() === charName ||
+      char.nickname.toLowerCase() === charName
+  );
+
+  res.render("index", { charResults });
+});
 
 const paths = {
   saying: "Nobody doesn't like Molten Boron!",
@@ -14,17 +39,7 @@ app.get("/:path", (req, res) => {
       paths[path] || "I am Bender, please insert girder (file path not found)"
   });
 });
-
-// app.get("/saying", (req, res) => {
-//   res.json({ saying: "Nobody doesn't like Molten Boron!" });
-// });
-// app.get("/popplers", (req, res) => {
-//   res.json({
-//     popplers: "Pop a Poppler in your mouth when you eat a Fishy Joe's!"
-//   });
-// });
 const port = process.env.PORT || 3000;
-
 app.listen(3000, () => {
   console.log(`Running on port ${port}`);
 });
