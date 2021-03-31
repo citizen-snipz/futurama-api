f9b1c3987a142db2e4ab3ba1e64530f1bbf34535
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-30 21:10:21 -0700

    updated charResults, set up remote heroku

--- server.js
+++ server.js
@@ -18,27 +18,29 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   let { charName } = req.query;
+
+  if (!charName) return res.render("index", { charResults: null });
   charName = charName.toLowerCase();
   const charResults = characters.filter(
     (char) =>
       char.name.toLowerCase() === charName ||
       char.nickname.toLowerCase() === charName
   );
-
-  res.render("index", { charResults });
+  if (charResults[0]) return res.render("index", { charResults });
+  res.render("index", { charResults: null });
 });
 
-const paths = {
-  saying: "Nobody doesn't like Molten Boron!",
-  popplers: "Pop a Poppler in your mouth when you eat a Fishy Joe's!"
-};
-app.get("/:path", (req, res) => {
-  const path = req.params.path;
-  res.json({
-    statement:
-      paths[path] || "I am Bender, please insert girder (file path not found)"
-  });
-});
+// const paths = {
+//   saying: "Nobody doesn't like Molten Boron!",
+//   popplers: "Pop a Poppler in your mouth when you eat a Fishy Joe's!"
+// };
+// app.get("/:path", (req, res) => {
+//   const path = req.params.path;
+//   res.json({
+//     statement:
+//       paths[path] || "I am Bender, please insert girder (file path not found)"
+//   });
+// });
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`Running on port ${port}`);
