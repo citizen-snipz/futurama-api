bdc0f54bfae4905ea7111c1202abed3a3ba2c5cd
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-16 17:24:27 -0700

    removed event listener, didn't need with form

--- public/main.js
+++ public/main.js
@@ -1,16 +1,1 @@
 const search = document.querySelector(".submitBtn");
-
-search.addEventListener("click", getCharResults);
-
-async function getCharResults() {
-  const character = document.querySelector(".search").value;
-  try {
-    console.log(character);
-    const res = await fetch("/getCharResults", {
-      method: "get",
-      headers: { "Content-Type": "application/json" }
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
