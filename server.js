5a1330f0511459e49ef3bfa9394e08061a9bb66c
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-16 17:42:34 -0700

    deleted require keys.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const { user, mdbKey } = require("./keys.js");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
